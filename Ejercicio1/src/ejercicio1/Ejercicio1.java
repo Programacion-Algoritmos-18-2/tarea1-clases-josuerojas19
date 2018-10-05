c2ef3ab1adf5eefe76132fb6019b68fe618c023d
Josue Rojas <jsebastianrojas19@gmail.com>
2018-10-05 11:30:26 -0500

    Subiendo tarea 1

--- Ejercicio1/src/ejercicio1/Ejercicio1.java
+++ Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -1,21 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ejercicio1;
+import java.util.Scanner; 
 
-/**
- *
- * @author Josue
- */
 public class Ejercicio1 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    Scanner teclado=new Scanner(System.in);
+    public static String persona1, persona2;
+    public void  ingnombres(){
+        System.out.println("Ingrese Nombres:");
+        System.out.println(" Estudiante  1: ");
+        persona1=teclado.next();
+        System.out.println(" Estudiante 2: ");
+        persona2=teclado.next();
     }
-    
+    public void presentar(){
+        System.out.println("Estudiante 1: ");
+        System.out.println("Noimbre: "+persona1);
+        System.out.println("Estudiante  2: ");
+        System.out.println("Noimbre: "+persona2);
+    }   
+}
+class Estudiante{
+    public static void main (String[]args){
+        Ejercicio1 ing=new Ejercicio1();
+        Ejercicio1 pre=new Ejercicio1();
+        ing.ingnombres();
+        pre.presentar(); 
+    }        
 }
